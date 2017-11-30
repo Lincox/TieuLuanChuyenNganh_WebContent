65aceb07765b460776a95843f3b77d6897d19fc3
Thang <thanguteit@gmail.com>
2017-11-30 21:38:58 +0700

    Thang_Comment
    2017-11-30

---
+++ Thang_2017-11-30/LoginTest/LoginTest/Models/BinhLuan.cs
@@ -0,0 +1,50 @@
+namespace LoginTest.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("BinhLuan")]
+    public partial class BinhLuan
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public BinhLuan()
+        {
+            BinhLuan1 = new HashSet<BinhLuan>();
+        }
+
+        [Key]
+        [Column("_idBinhLuan")]
+        [StringLength(50)]
+        public string C_idBinhLuan { get; set; }
+
+        [Column("_idUserDoc")]
+        [StringLength(50)]
+        public string C_idUserDoc { get; set; }
+
+        public DateTime? ThoiDiemBinhLuan { get; set; }
+
+        [Column("_idBaiViet")]
+        [StringLength(50)]
+        public string C_idBaiViet { get; set; }
+
+        public string NoiDungBL { get; set; }
+
+        [Column("_idBinhLuanGoc")]
+        [StringLength(50)]
+        public string C_idBinhLuanGoc { get; set; }
+
+        public int? CapBac { get; set; }
+
+        public virtual BaiViet BaiViet { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<BinhLuan> BinhLuan1 { get; set; }
+
+        public virtual BinhLuan BinhLuan2 { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
